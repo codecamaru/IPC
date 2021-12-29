f557a90243894d000c431c01123727c37d7e8347
Carolina Marugan Rubio <54485798+codecamaru@users.noreply.github.com>
2021-12-30 00:30:08 +0100

    Add files via upload

---
+++ src/poiupv/EstadisticasController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+
+/**
+ * FXML Controller class
+ *
+ * @author carolinaalbamaruganrubio
+ */
+public class EstadisticasController implements Initializable {
+
+    @FXML
+    private PieChart pie;
+    @FXML
+    private BarChart<?, ?> bar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
